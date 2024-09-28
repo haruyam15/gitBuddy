a597be3d2aac7aeac673015a4a76a50bc4d3d5b0
Haeun <haruyam15@gmail.com>
2024-09-28 11:50:09 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    eqeqeq: 'error',
+    'import/no-default-export': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-useless-rename': 'error',
+    'object-shorthand': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+        paths: ['src'],
+      },
+    },
+  },
+};
